463dc325aeff5537688e32717fd2169eb7f3df8d
Yaroslav <tvoibaget@mail.ru>
2023-10-23 13:58:04 +0300

    fix: prettier fix

--- src/helpers/getDataFromLocalStorage.js
+++ src/helpers/getDataFromLocalStorage.js
@@ -1,7 +1,6 @@
 const getDataFromLocalStorage = () => {
-    const lastUpdated =
-        localStorage.getItem('lastUpdated') || '2023-09-19T00:00:00Z';
-    const data = JSON.parse(localStorage.getItem('data')) || null;
-    return { lastUpdated, data };
+  const lastUpdated = localStorage.getItem('lastUpdated') || '2023-09-19T00:00:00Z';
+  const data = JSON.parse(localStorage.getItem('data')) || null;
+  return { lastUpdated, data };
 };
 export default getDataFromLocalStorage;
